bcd1144ae3032539698127ca980e766bafef6891
ReinaldoMantovai <reinaldotha88@gmail.com>
2025-03-20 17:41:41 -0300

    refact: refecting AddUsuarioView

--- src/main/java/br/com/reinaldo/views/addusuario/AddUsuarioView.java
+++ src/main/java/br/com/reinaldo/views/addusuario/AddUsuarioView.java
@@ -10,10 +10,15 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.UploadI18N;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.PropertyId;
@@ -23,6 +28,12 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 
 @PageTitle("Cadastrar usuario")
 @Route(value = "cadastrar-usuario")
@@ -46,6 +57,7 @@ public class AddUsuarioView extends VerticalLayout {
 	private TextField rg;
 	@PropertyId("tipoEmitente")
 	private ComboBox<TipoEmitenteEnum> tipoEmitenteEnum;
+	private Upload profileImageUrl;
 
 	private Button saveButton;
 	private Button cancelButton;
@@ -85,12 +97,14 @@ public class AddUsuarioView extends VerticalLayout {
 		cpf = new TextField("CPF");
 		rg = new TextField("RG");
 
+
 		// Criação do ComboBox
 		tipoEmitenteEnum = new ComboBox<>("Tipo Emitente");
 		tipoEmitenteEnum.setItems(TipoEmitenteEnum.values()); // Preenchendo com os valores do enum
 		tipoEmitenteEnum.setItemLabelGenerator(TipoEmitenteEnum::name); // Usando o nome do enum como rótulo
 
 		saveButton = new Button("Salvar");
+
 		cancelButton = new Button("Cancelar");
 
 		// Adicionando listeners aos botões
@@ -98,9 +112,40 @@ public class AddUsuarioView extends VerticalLayout {
 		saveButton.addClickListener(e -> saveUsuariosDto());
 	}
 
+	private Component uploadImage() {
+		MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
+		profileImageUrl = new Upload(buffer);
+
+		profileImageUrl.setAutoUpload(true);
+
+		// Inicializando o UploadI18N e o AddFiles
+		UploadI18N i18n = new UploadI18N();
+		UploadI18N.AddFiles addFiles = new UploadI18N.AddFiles();
+		addFiles.setMany("Upload Imagem");
+		i18n.setAddFiles(addFiles); // Definindo o objeto AddFiles no UploadI18N
+
+		profileImageUrl.setI18n(i18n);
+
+		// Adicionando um listener para processar o arquivo após o upload
+		profileImageUrl.addSucceededListener(event -> {
+			String fileName = event.getFileName();
+			InputStream inputStream = buffer.getInputStream(event.getFileName());
+
+			// Aqui você pode salvar o arquivo ou fazer o que for necessário
+			try {
+				Files.copy(inputStream, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+				Notification.show("Erro ao salvar a imagem: " + e.getMessage(), 3000, Notification.Position.MIDDLE);
+			}
+		});
+
+		return profileImageUrl;
+	}
+
 	private Component createFormLayout() {
 		FormLayout form = new FormLayout();
-		form.add(nome, sobrenome,username, email ,password ,statusUsuario, cpf, rg, tipoEmitenteEnum, createButtons());
+		form.add(nome, sobrenome,username, email ,password ,statusUsuario, cpf, rg, tipoEmitenteEnum,uploadImage(),createButtons());
 		form.setColspan(nome, 1);
 		form.setResponsiveSteps(
 				// Use one column by default
@@ -117,10 +162,15 @@ public class AddUsuarioView extends VerticalLayout {
 	}
 
 	private void saveUsuariosDto() {
+		Notification notification = new Notification();
 		try {
 			binder.writeBean(usuario);
 			usuariosService.createUsuario(usuario);
+
 			clearField();
+			notification.add("Usuário cadastrado com sucesso!");
+			notification.setPosition(Notification.Position.TOP_CENTER);
+			notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
 		} catch (ValidationException | IOException e) {
 			e.printStackTrace();
 		}
